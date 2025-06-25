e93ca76305ebf109a6de28eabe83ac3e585b58d8
ganatan <dannyganatan@gmail.com>
2025-06-25 16:28:07 +0200

    Update Angular 20.0.4 & Angular CLI 20.0.3

--- frontend-angular/src/environments/environment.ts
+++ frontend-angular/src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     application:
     {
       name: 'angular-bootstrap',
-      version: 'Angular 20.0.3',
-      bootstrap: 'Bootstrap 5.3.6',
+      version: 'Angular 20.0.4',
+      bootstrap: 'Bootstrap 5.3.7',
       fontawesome: 'Font Awesome 6.7.2',
     }
   };

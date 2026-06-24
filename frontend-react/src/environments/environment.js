ac373d24c61de339542d9e66edb3052b29027327
ganatan <dannyganatan@gmail.com>
2026-06-24 10:56:33 +0200

    improve Angular 22.0.3

--- frontend-react/src/environments/environment.js
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  application:
-  {
-    name: 'react-bootstrap',
-    version: 'React 18.2.0',
-    bootstrap: 'Bootstrap 5.2.2',
-    fontawesome: 'Font Awesome 6.2.0',
-  }
-};
